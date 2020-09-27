32af6d16b861795e2e58dc9bd42db6bec60375be
stipple3d <hutherb@gmail.com>
2020-09-26 20:18:12 -0400

    added beginnings of a pathfinding project to be based off of the A* method (4dir for now, with diagonals as option)

---
+++ projects/src/pathfindingA_init.js
@@ -0,0 +1,14 @@
+let canvas, context;
+let s3dLoop;
+let game;
+function Setup(){
+	game = new PathfindingGameA();
+	s3dLoop = new s3dGameLoop(1, game.update, game.render);
+	s3dLoop.startLoop();
+}
+//HTML event listener
+document.addEventListener("DOMContentLoaded", function(){
+	canvas = document.getElementById("myCanvas");
+	context = canvas.getContext("2d");
+    Setup();
+});
--- s3d/gameLoop.js
+++ s3d/gameLoop.js
@@ -23,68 +23,69 @@ class s3dGameLoop{
 		this.raf;
 
 		this.updated = false;
-
-		//NOTE: 'time' is coming from the requestAnimationFrame (event?)
-		//		(it will be ms since the game was loaded)
-		this.processFrame = (time) => {
-			//add the time since last update to accumulated time counter 
-			//(converted to seconds, to match our this.dt var units)
-			this.accTime += (time - this.lastTime) /1000;
-
-			//console.log('rafTime: ' + time + ', last: ' + this.lastTime);
-
-			while(this.accTime >= this.dt){
-
-				//TODO: run update callback
-				if(this.updateCB != undefined){
-					this.updateCB(this.dt);//passing out DeltaTime in the update
-				}
-				//console.log('update: ' + this.updateCB);
-
-				//decrement the accumulated time counter by the value of deltaTime
-				this.accTime -= this.dt;
-
-				//mark that we updated in this processFrame
-				this.updated = true;
-			}
-
-			//if we updated, we should also render this processFrame
-			if(this.updated){
-
-				//TODO: run the render callback
-				if(this.renderCB != undefined){
-					this.renderCB();
-				}
-				//console.log('render: ' +  this.renderCB);
-
-				//mark the updated flag as false again for the next process frame
-				this.updated = false;
-			}
-
-			//store this processFrame's time as the last time
-			this.lastTime = time;
-
-			//run function to request a frame again
-			this.requestAFrame();
-		}
-
-		this.startLoop = function(){
-			//set the time we are starting the loop as the 
-			//previous time (so it will not try to process all the frames
-			//since it was stopped in the next frame)
-			this.lastTime = window.performance.now();
-			//run function to request a frame again
-			this.requestAFrame();
-		}
-
-		this.requestAFrame = function(){
-			//calls and stores a requestanimationframe
-			this.raf = window.requestAnimationFrame(this.processFrame);
-		}
-
-		this.stopLoop = function(){
-			//cancel the stored requestanimationframe
-			window.cancelAnimationFrame(this.raf);
-		}
 	}
+
+	//NOTE: 'time' is coming from the requestAnimationFrame (event?)
+	//		(it will be ms since the game was loaded)
+	processFrame = (time) => {
+		//add the time since last update to accumulated time counter 
+		//(converted to seconds, to match our this.dt var units)
+		this.accTime += (time - this.lastTime) /1000;
+
+		//console.log('rafTime: ' + time + ', last: ' + this.lastTime);
+
+		while(this.accTime >= this.dt){
+
+			//TODO: run update callback
+			if(this.updateCB != undefined){
+				this.updateCB(this.dt);//passing out DeltaTime in the update
+			}
+			//console.log('update: ' + this.updateCB);
+
+			//decrement the accumulated time counter by the value of deltaTime
+			this.accTime -= this.dt;
+
+			//mark that we updated in this processFrame
+			this.updated = true;
+		}
+
+		//if we updated, we should also render this processFrame
+		if(this.updated){
+
+			//TODO: run the render callback
+			if(this.renderCB != undefined){
+				this.renderCB();
+			}
+			//console.log('render: ' +  this.renderCB);
+
+			//mark the updated flag as false again for the next process frame
+			this.updated = false;
+		}
+
+		//store this processFrame's time as the last time
+		this.lastTime = time;
+
+		//run function to request a frame again
+		this.requestAFrame();
+	}
+
+	startLoop = function(){
+		//set the time we are starting the loop as the 
+		//previous time (so it will not try to process all the frames
+		//since it was stopped in the next frame)
+		this.lastTime = window.performance.now();
+		//run function to request a frame again
+		this.requestAFrame();
+	}
+
+	requestAFrame = function(){
+		//calls and stores a requestanimationframe
+		this.raf = window.requestAnimationFrame(this.processFrame);
+	}
+
+	stopLoop = function(){
+		//cancel the stored requestanimationframe
+		window.cancelAnimationFrame(this.raf);
+	}
+	
 }
